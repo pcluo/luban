cd8b00ae6650a7b4999712258f18b50ef17a4f31
Patrick Luo <cluoren@gmail.com>
2017-07-02 15:44:59 -0400

    add init file

---
+++ init_inline.py
@@ -0,0 +1,26 @@
+import sys,os, re
+# sys.path.append('/Users/cluo/python')
+# sys.path.append('/Users/cluo/wyss')
+
+import pandas as pd
+from numpy import *
+from pandas import *
+from pandas import IndexSlice as idx
+get_ipython().magic(u'matplotlib inline')
+import seaborn as sns
+# import Quandl
+
+# import statsmodels.api as sm
+import statsmodels.formula.api as sm
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = [6,4]
+
+from importlib import reload
+# import utils.monkey as mk
+
+
+def add_path(path2add):
+    if path2add not in sys.path:
+        sys.path.append(path2add)
